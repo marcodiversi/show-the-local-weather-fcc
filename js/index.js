0d15cfd3b156c3b39cf4f4d08e6ae13f8e4a25dd
Marco Diversi <marcodiversi@gmail.com>
2017-03-10 10:49:14 +0000

    https working

--- js/index.js
+++ js/index.js
@@ -1,40 +1,99 @@
-/* <!-- REMOVE THE s FROM THE HTTPS LINK TO MAKE IT WORK EXAMPLE https://codepen.. has to be http://codepen.... -->*/
-
+/*A local weather web-app that generates your local weather based on your IP. 
+ */
 $(document).ready(function() {
 
-  var url = "http://ip-api.com/json";
-  $.getJSON(url, function(data) {
-    console.log(data);
-    var city = data.city;
-    var cCode = data.countryCode;
-    var country = data.country;
-    $('#city').html(city);
-    $('#country').html(country);
-
-    var url2 = "http://api.openweathermap.org/data/2.5/forecast/city?q=" + city + "," + cCode + "+&APPID=0e8682a11cf28a7f98e8593f48df8c16&units=metric";
-
-    $.getJSON(url2, function(data2) {
-      console.log(data2);
-      var temp = Math.round(data2.list[0].main.temp);
-
-      if (temp > 25) {
-        $('body').css('background-image', 'url(http://bit.ly/2k2tu2l)');
-      } else if (temp < 25 & temp > 5) {
-        $('body').css('background-image', 'url(http://bit.ly/2kE66J3)');
-      } else if (temp < 5) {
-        $('body').css('background-image', 'url(http://bit.ly/2komBZF)');
-      }
-
-   $('#temp').html(temp + " °C");
-      
-   $('#F').click(function() {
-   $('#temp').html(Math.round((temp * (9 / 5) + 32)) + " °F");
-
-   $('#C').click(function() {
-   $('#temp').html(temp + " °C");
-
-        });
-      });
-    });
+  //get location by ip sadasda
+  $.ajax({
+    type: 'GET',
+    url: 'https://ipinfo.io/json/',
+    success: coordinates
   });
+  //coordinates callback
+
+  function coordinates(point) {
+    var coords = point.loc.split(',');
+    var lat = parseFloat(coords[0]);
+    var lon = parseFloat(coords[1]);
+    var city = point.city;
+    var region = point.region;
+    var country = point.country;
+
+    //forecast.io api
+    var api = 'https://api.forecast.io/forecast/7c693e97458b09a53aa5f7e2cd0af4a0/' + lat + ',' + lon + '?units=si';
+
+    //display city, region and country
+    displayLocation(city, region, country);
+
+    //insert location into getWeather function
+    getWeather(api);
+
+  } //end coordinates
+
+  function displayLocation(city, region, country) {
+
+    //country code to full country
+    $.ajax({
+      type: 'GET',
+      url: 'https://restcountries.eu/rest/v1/alpha?codes=' + country,
+      success: function(data) {
+        //spit this out to website
+        $('#city').text(city + ', ' + region);
+        $('#country').text(data[0].name);
+      }
+    });
+  } //end displayLocation
+
+  //get the  weather
+  function getWeather(url) {
+    $.ajax({
+      type: 'GET',
+      url: url,
+      dataType: 'jsonp',
+      success: weather
+    });
+
+    function weather(data) {
+
+      //main forecast
+      var temp = Math.round(data.currently.temperature),
+        conditions = data.currently.icon.split('-').join(' '),
+        icon = data.currently.icon,
+        low = Math.round(data.daily.data[0].temperatureMin),
+        high = Math.round(data.daily.data[0].temperatureMax);
+
+     
+
+      //display weather on website
+      displayWeather(icon, temp.toFixed(0), conditions, low, high);
+
+      //convert daily units
+      $('#F').on('click', function() {
+        displayWeather(icon, toFah(temp.toFixed(0)));
+        $('#F').prop('disabled', true);
+        $('#C').prop('disabled', false);
+
+      });
+
+      $('#C').on('click', function() {
+
+        displayWeather(icon, temp.toFixed(0));
+        $('#C').prop('disabled', true);
+        $('#F').prop('disabled', false);
+       
+      });
+    } 
+
+    function displayWeather(icon, temp, condition, low, high) {
+      //plop to website
+
+      $('#temp').text(temp + '\xB0');
+
+    } //end displayWeather
+
+    function toFah(temp) {
+      return parseInt(temp * (9 / 5) + 32);
+    } //end convert to fahrenheit
+
+  } //end getWeather
+
 });
